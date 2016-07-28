9bf6f83bab81cd677b67dfa0a14738f47119f849
Leo Schweizer <leonhard.schweizer@d-labs.com>
2016-07-28 10:34:55 +0200

    initial config setup

---
+++ index.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+
+	extends : [
+		'standard'
+	],
+
+	rules: {
+		'quotes': [2, 'single', 'avoid-escape'],
+		'semi': [2, 'always'],
+		'max-len': [2, 120, 2],
+		'indent': [2, 'tab'],
+		'padded-blocks': [0],
+		'space-before-function-paren': [2, 'never']
+	}
+
+};
